f0c3c9296c04f017fbd6dadba38f8c74238f9835
EC2 Default User <ec2-user@ip-172-31-17-86.ec2.internal>
2022-02-10 00:18:51 +0000

    [Finish] 4-11

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -7,20 +7,21 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
-    redirect_to post_image_psth
+    redirect_to post_images_path
   end
 
   def index
+    @post_images = PostImage.all
   end
 
   def show
   end
-  
+
   def destroy
   end
-  
+
   private
-  
+
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
